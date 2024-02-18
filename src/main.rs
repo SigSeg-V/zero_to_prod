ab8ca4f11cdffb654617605238b59be38b0775de
Charlie <30825964+Vxlnce@users.noreply.github.com>
2024-02-19 08:51:10 +1100

    Added greeting response

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use tide::prelude::*;
+use tide::Request;
+
+#[async_std::main]
+async fn main() -> tide::Result<()> {
+    let mut app = tide::new();
+    app.at("/").get(greeting);
+    app.at("/:name").get(greeting);
+    app.listen("127.0.0.1:8080").await?;
+    Ok(())
+}
+
+async fn greeting(request: Request<()>) -> tide::Result<String> {
+    let name = request
+        .param("name")
+        .unwrap_or("World");
+
+    Ok(format!("Hello, {}!", name))
 }
